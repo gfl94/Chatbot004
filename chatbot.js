d726bdf0b1862e11a0a558f6147e1b58db80971b
Gefei Li <gefeili.2013@gmail.com>
2023-03-28 00:03:22 +0800

    minor code refactor (#3)
    * refactor onMessage function
    
    * fix log format issue
    
    * Update README.md
    
    * uncomment another group
    
    * add gitignore
    
    * signing change
    
    * Update README.md

--- chatbot.js
+++ chatbot.js
@@ -26,7 +26,6 @@ var magic_word = default_magic_word
 var admin_username = ""
 function update_magic_word(new_magic_word) {
    log.info(`update magic word from ${magic_word} to ${new_magic_word}`)
-   log.info(`word type ${typeof(new_magic_word)}`)
    magic_word = new_magic_word
 }
 function update_admin_user(username) {
@@ -35,7 +34,6 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
     admin_username = username
 }
 function is_admin_user(username) {
@@ -51,73 +49,87 @@ function onScan (qrcode, status) {
 			encodeURIComponent(qrcode),
 		].join('')
 
-		log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
+		log.info('GPTBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
 
 	} else {
-		log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
+		log.info('GPTBot', 'onScan: %s(%s)', ScanStatus[status], status)
 	}
 }
 
 function onLogin (user) {
     update_magic_word(user.name() + "-chatbot")
     update_admin_user(user.name())
-	log.info('StarterBot', '%s login', user)
+	log.info('GPTBot', '%s login', user)
 }
 
 function onLogout (user) {
-	log.info('StarterBot', '%s logout', user)
+	log.info('GPTBot', '%s logout', user)
+}
+
+async function sendBack(contact, msgId, message) {
+    log.info(`send_back [${msgId}] ${message}`)
+    try {
+        await contact.say(message);
+    } catch (e) {
+        log.error(`${msgId} say back error ${e}`)
+    }
 }
 
 async function onMessage (msg) {
     log.info(`onMessage [${msg.id}] msg ${msg.toString()}`);
-    const roomName = await msg.room()?.topic();
 
-    if (msg.text() === 'ding') {
-        log.info("send_back dong")
-        await msg.say('dong')
-    } else if (msg.text() == "debug" && is_admin_user(msg.talker().name()) ) {
-        const res = "magic word: " + magic_word + "\nadmin user: " + admin_username;
-        log.info(res)
-        await msg.say(res)
+    const room = msg.room()
+    const talker = msg.talker()
+    const content = msg.text()
+
+    if (content == 'ding') {
+        await sendBack(msg, msg.id, "dong")
+    } else if (content == "debug" && is_admin_user(talker.name()) ) {
+        await sendBack(msg, msg.id, `magic word: ${magic_word}\nadmin user: ${admin_username}`)
     }
-    else if (is_admin_user(msg.talker().name()) && msg.text().includes("change magic word ")) {
-        const new_magic_word = msg.text().replace("change magic word ", "")
+    else if (is_admin_user(talker.name()) && content.includes("change magic word ")) {
+        const new_magic_word = content.replace("change magic word ", "")
         update_magic_word(new_magic_word)
-        const response = "updated magic word to " + new_magic_word
-        log.info("send_back " + response)
-        await msg.say(response)
+        await sendBack(msg, msg.id `updated magic word to ${new_magic_word}`)
     }
-    else if (msg.text().includes(magic_word)) {
-        const response = await sendMessageToChatGPT(generatePrompt(msg.text().replace(magic_word, "")))
-        log.info("send_back " + response)
-        await msg.say(response + "\n from ChatGPT")
-    } else if (roomName == "ChatGPT语音测试" && msg.type() == bot.Message.Type.Audio) {
-        const fileBox = await msg.toFileBox();
-        const fileName =  "voice_tmp/" + fileBox.name;
-        if (fs.existsSync(fileName)) {
-            log.info(`[${msg.id}][onVoice] voice file already exists, skip`);
-            return;
-        }
-        await fileBox.toFile( fileName, true);
-        const readStream = await fs.createReadStream(`${fileName}`);
-        const response = await openai.createTranscription(
-            readStream,
-            "whisper-1"
-        );
-        if (response.data.text != undefined) {
-            const sentence = response.data.text;
 
-            if (sentence.includes("笨笨") || sentence.includes("本本") ) {
-                var words = sentence.replace("笨笨", "").replace("本本", "");
-                const chat_response = await sendMessageToChatGPT(words);
-                log.info("send_back " + chat_response);
-                await msg.say(`${chat_response} \n from ChatGPT`, msg.talker())
-            } else {
-                log.info(`[${msg.id}][onVoice] output sentence from openai is ${sentence}`);
-                msg.room().say(`your message is "${sentence}" \n\n\n    from ChatGPT语音模型, 请在语音内容里加上"笨笨"召唤ChatGPT给你回答`, msg.talker());
+    if (room) {
+        const topic = await room.topic();
+
+        if ((topic.includes("GPT test") || topic == "ChatGPT语音测试")){
+            if (msg.type() == bot.Message.Type.Text && content.includes(magic_word)) {
+                const response = await sendMessageToChatGPT(generatePrompt(content.replace(magic_word, "")))
+                await sendBack(room, msg.id, `${response} \n from ChatGPT`)
+            } else if (msg.type() == bot.Message.Type.Audio) {
+                const fileBox = await msg.toFileBox();
+
+                const fileName =  "voice_tmp/" + fileBox.name;
+                if (fs.existsSync(fileName)) {
+                    log.info(`[${msg.id}][onVoice] voice file already exists, skip`);
+                    return;
+                }
+                await fileBox.toFile( fileName, true);
+                const readStream = await fs.createReadStream(`${fileName}`);
+                //const readStream = await fileBox.toStream();
+                const response = await openai.createTranscription(
+                    readStream,
+                    "whisper-1"
+                );
+                if (response.data.text != undefined) {
+                    const sentence = response.data.text;
+
+                    if (sentence.includes("笨笨") || sentence.includes("本本") ) {
+                        var words = sentence.replace("笨笨", "").replace("本本", "");
+                        const chat_response = await sendMessageToChatGPT(words);
+                        sendBack(room, msg.id, `${chat_response} \n from ChatGPT`);
+                    } else {
+                        log.info(`[${msg.id}][onVoice] output sentence from openai is ${sentence}`);
+                        sendBack(room, msg.id, `your message is '${sentence}' \n\n\n from ChatGPT语音模型，请在语音内容里加上'笨笨'召唤ChatGPT给你回答`);
+                    }
+                } else {
+                    log.info(`[${msg.id}][onVoice] trying to print error ${response.data.error}`);
+                }
             }
-        } else {
-            log.info(`[${msg.id}][onVoice] trying to print error ${response.data.error}`);
         }
     }
 }
@@ -146,5 +158,5 @@ bot.on('logout',  onLogout)
 bot.on('message', onMessage)
 
 bot.start()
-	.then(() => log.info('StarterBot', 'Starter Bot Started.'))
-	.catch(e => log.error('StarterBot', e))
+	.then(() => log.info('GPTBot', 'ChatGPT Bot Started.'))
+	.catch(e => log.error('GPTBot', e))
